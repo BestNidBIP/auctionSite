cb1e106fe1bea724203fd499517e8a8444e879b4
Belen <belendominguez@gmail.com>
2015-05-23 13:07:36 -0300

    Added profile page

--- bestnid/packages/custom/profile/public/routes/profile.js
+++ bestnid/packages/custom/profile/public/routes/profile.js
@@ -3,7 +3,7 @@
 angular.module('mean.profile').config(['$stateProvider',
   function($stateProvider) {
     $stateProvider.state('profile example page', {
-      url: '/profile/example',
+      url: '/profile',
       templateUrl: 'profile/views/index.html'
     });
   }
