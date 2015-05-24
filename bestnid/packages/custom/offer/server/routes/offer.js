b6c498b9a7c4ba7dd5f2159a1d808c80f85e1964
Federico M. Peretti <federicoperetti@gmail.com>
2015-05-23 22:06:42 -0300

    Offers service. Now the endpoint /offer acceps POST and GET methods.

--- bestnid/packages/custom/offer/server/routes/offer.js
+++ bestnid/packages/custom/offer/server/routes/offer.js
@@ -1,20 +1,16 @@
 'use strict';
 
+var offer = require('../controllers/offer'),
+    config = require('meanio').loadConfig();
+
 /* jshint -W098 */
 // The Package is past automatically as first parameter
 module.exports = function(Offer, app, auth, database) {
 
-  app.get('/offer/example/anyone', function(req, res, next) {
-    res.send('Anyone can access this');
-  });
+  app.route('/offer')
+    .get(offer.all)
+    .post(offer.create);
 
-  app.get('/offer/example/auth', auth.requiresLogin, function(req, res, next) {
-    res.send('Only authenticated users can access this');
-  });
-
-  app.get('/offer/example/admin', auth.requiresAdmin, function(req, res, next) {
-    res.send('Only users with Admin role can access this');
-  });
 
   app.get('/offer/example/render', function(req, res, next) {
     Offer.render('index', {
