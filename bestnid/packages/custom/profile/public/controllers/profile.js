40268fbc2103c5ac2a377c38308a6b3257e33a6f
Belen <belendominguez@gmail.com>
2015-05-25 14:06:30 -0300

    Layout for publications

--- bestnid/packages/custom/profile/public/controllers/profile.js
+++ bestnid/packages/custom/profile/public/controllers/profile.js
@@ -23,10 +23,6 @@ angular.module('mean.profile').controller('ProfileController', ['$scope', 'Globa
             }
         ];
 
-        $scope.setPage = function () {
-            $state.transitionTo(page);
-        };
-
         $scope.user_data = null;
 
         $scope.user_data_promise = Profile.getProfile();
@@ -34,6 +30,26 @@ angular.module('mean.profile').controller('ProfileController', ['$scope', 'Globa
         $scope.user_data_promise.then(function (response) {
             $scope.user_data = response.data;
         });
+
+        $scope.master = {};
+
+        $scope.updateUser = function (user) {
+            $scope.master = angular.copy(user);
+        };
+
+        // Lo que debería de traer el servicio de publicaciones
+        $scope.publication_data = [
+            {
+                title: 'Bicicleta plegable',
+                imgPath: 'http://labicikleta.com/wp-content/uploads/2013/07/Viajes.jpg',
+                daysRemained: 5
+            },
+            {
+                title: 'Remera nirvana',
+                imgPath: 'http://mlu-s2-p.mlstatic.com/remeras-de-nirvana-estampadas-en-serigrafia-1668-MLU3418072179_112012-O.jpg',
+                daysRemained: 10
+            }
+        ];
 
     }
 ]);
