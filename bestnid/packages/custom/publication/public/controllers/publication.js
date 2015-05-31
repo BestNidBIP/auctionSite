06b8d85b612403ba02075f0255ace15a806f3adf
belen <belendominguez@gmail.com>
2015-05-31 19:35:08 -0300

    Se pueden agregar publicaciones

--- bestnid/packages/custom/publication/public/controllers/publication.js
+++ bestnid/packages/custom/publication/public/controllers/publication.js
@@ -7,5 +7,21 @@ angular.module('mean.publication').controller('PublicationController', ['$scope'
     $scope.package = {
       name: 'publication'
     };
+
+    // Get categories available
+    $scope.categories = null;
+    $scope.categories_promise = Publication.getCategories();
+    $scope.categories_promise.then(function (response) {
+      $scope.categories = response.data;
+    });
+
+
+      // Send new publications
+      $scope.submit_form = function(publication){
+          $scope.submit_form_promise = Publication.addNewPublication(publication);
+          $scope.submit_form_promise.then( function (response) {
+              $scope.response_form = response.data;
+          });
+      };
   }
 ]);
--- bestnid/packages/custom/publication/public/services/publication.js
+++ bestnid/packages/custom/publication/public/services/publication.js
@@ -1,9 +1,21 @@
 'use strict';
 
-angular.module('mean.publication').factory('Publication', [
-  function() {
+angular.module('mean.publication').factory('Publication', [ '$http',
+  function($http) {
+
+    function _getCategories(){
+      return $http.get('/category')
+    }
+
+      function _addNewPublication(publication_data){
+        return $http.post('/publication', publication_data)
+      }
+
+
     return {
-      name: 'publication'
+      name: 'publication',
+      getCategories: _getCategories,
+        addNewPublication: _addNewPublication
     };
   }
 ]);
