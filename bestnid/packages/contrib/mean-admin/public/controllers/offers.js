58a695e6dd6e13ec6e5c4e91baede3c6a7d19882
belen <belendominguez@gmail.com>
2015-06-21 11:38:41 -0300

    Offers

---
+++ bestnid/packages/contrib/mean-admin/public/controllers/offers.js
@@ -0,0 +1,6 @@
+angular.module('mean.mean-admin').controller('OfferAdminController', 
+	['$scope', 'Global', '$rootScope', '$http', 'OffersAdmin',
+    function($scope, Global, $rootScope, $http, OffersAdmin) {
+        
+    }
+]);
---
+++ bestnid/packages/contrib/mean-admin/public/services/offers.js
@@ -0,0 +1,14 @@
+//Users service used for users REST endpoint
+angular.module('mean.mean-admin').factory("OffersAdmin", ['$http',
+    function($http) {
+
+    function _getOffers(){
+      
+    }
+
+    return {
+      name: 'offersAdmin',
+      
+    };
+  }
+]);
