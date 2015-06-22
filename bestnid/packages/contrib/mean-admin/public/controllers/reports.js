09a3cd4dd0cc9c2b05543d6dee23b71e3e82221c
belen <belendominguez@gmail.com>
2015-06-22 19:34:33 -0300

    Fix para el fefi

--- bestnid/packages/contrib/mean-admin/public/controllers/reports.js
+++ bestnid/packages/contrib/mean-admin/public/controllers/reports.js
@@ -13,39 +13,13 @@ angular.module('mean.mean-admin').controller('reportsController',
         $scope.users_report_promise.then(function(response){
          $scope.users_report = response.data; 
        });
-      }
-
+      };
 
       $scope.sales_report = function(date) {
         $scope.sales_report_promise = reports.getSalesByDate(date);
         $scope.sales_report_promise.then(function(response){
           $scope.sales_report_data = response.data; 
-       }
-     });
-
-        $scope.groups = [
-        {
-          title: 'Dynamic Group Header - 1',
-          content: 'Dynamic Group Body - 1'
-        },
-        {
-          title: 'Dynamic Group Header - 2',
-          content: 'Dynamic Group Body - 2'
-        }
-        ];
-
-        $scope.items = ['Item 1', 'Item 2', 'Item 3'];
-
-        $scope.addItem = function() {
-          var newItemNo = $scope.items.length + 1;
-          $scope.items.push('Item ' + newItemNo);
-        };
-
-        $scope.status = {
-          isFirstOpen: true,
-          isFirstDisabled: false
-        };
-
-
-      }
-      ]);
+        });
+      };
+      
+     } ]);
